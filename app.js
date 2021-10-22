49d011f8e8c8d97cc490145ff1cecf8565d85288
James Robbins <James@Thomass-MacBook-Pro.local>
2021-10-22 09:57:04 +0900

    Added hello form and footer/header layout

--- app.js
+++ app.js
@@ -1,27 +1,24 @@
 const express =  require('express');
 
 const app = express();
-const colors = [
-    'red',
-    'orange',
-    'yellow',
-    'green',
-    'blue',
-    'purple'
-];
 
 app.set('view engine', 'pug');
+app.use(express.urlencoded());
 
 app.get('/', (req, res) => {
     res.render('index');
 });
 
 app.get('/cards', (req, res) => {
-    res.render('card', { prompt: "Whos is buried in Grant's tomb?", colors});
+    res.render('card', { prompt: "Whos is buried in Grant's tomb?"});
 });
 
 app.get('/sandbox', (req, res) => {
     res.render('sandbox');
+});
+
+app.all('/hello', (req, res) => {
+    res.render('hello', { name: req.body.username });
 });
 
 app.listen(3000, () => {
