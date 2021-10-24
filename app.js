896c61a3364fdc741cc0c8705c5c022e9a83cbe7
James Robbins <James@Thomass-MacBook-Pro.local>
2021-10-24 17:26:09 +0900

    received files from designer and integrated w/ pug

--- app.js
+++ app.js
@@ -1,53 +1,19 @@
 const express =  require('express');
-const cookieParser = require('cookie-parser')
+const cookieParser = require('cookie-parser');
 
 const app = express();
 
-app.set('view engine', 'pug');
 app.use(express.urlencoded());
 app.use(cookieParser());
+app.use('/static', express.static('public'));
 
+app.set('view engine', 'pug');
 
-app.get('/', (req, res) => {
-    const name = req.cookies.username;
-    if (name) {
-        res.render('index', { name });
-    } else {
-        res.redirect('/hello');
-    };
-});
+const mainRoutes = require('./routes');
+const cardRoutes = require('./routes/cards')
 
-app.post('/goodbye', (req, res) => {
-    res.clearCookie('username');
-    res.redirect('hello');
-})
-
-app.get('/cards', (req, res) => {
-    res.render('card', { prompt: "Whos is buried in Grant's tomb?"});
-});
-
-app.get('/sandbox', (req, res) => {
-    res.render('sandbox');
-});
-
-app.get('/hello', (req, res) => {
-    const name = req.cookies.username;
-    if (name) {
-        res.redirect('/');
-    } else {
-    res.render('hello');
-    }
-});
-
-app.post('/hello', (req, res) => {
-    res.cookie('username', req.body.username);
-    res.redirect('/');
-});
-
-app.post('/goodbye', (req, res) => {
-    res.clearCookie('username');
-    res.redirect('/hello');
-});
+app.use(mainRoutes);
+app.use('/cards', cardRoutes);
 
 app.use((req, res, next) => {
     const err = new Error('Not Found');
---
+++ routes/cards.js
@@ -0,0 +1,34 @@
+const express = require("express")
+const router = express.Router()
+
+const {
+    data: { cards }
+} = require("../data/flashcardData.json")
+
+router.get("/", (req, res) => {
+    let id = Math.floor(Math.random() * cards.length)
+    res.redirect(`/cards/${id}`)
+})
+
+router.get("/:id", (req, res) => {
+    try {
+        let { side } = req.query
+        const { id } = req.params
+        if (!side || side.toLowerCase() !== "answer") {
+            side = "question"
+        }
+        res.locals = {
+            name: req.cookies.username,
+            id,
+            side,
+            text: cards[id][side],
+            hint: cards[id].hint
+        }
+        res.render(`card`)
+    } catch (err) {
+        console.error(err.message)
+        res.redirect("/cards")
+    }
+})
+
+module.exports = router
